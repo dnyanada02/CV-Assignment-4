f9f565e81b7b7a2b1f79eec6ec99abc2673d3ac1
dnyanada <71203168+dnyanada02@users.noreply.github.com>
2022-11-16 00:51:04 +0530

    Update 61_cv_assignment4.py

--- 61_cv_assignment4.py
+++ 61_cv_assignment4.py
@@ -33,11 +33,11 @@ def main():
 
     st.write(font_css, unsafe_allow_html=True)
 
-#     tabs = st.tabs(('Panorama Image'))
+     tabs = st.tabs(('Panorama Image'))
 
 
-#     with tabs[1]:
-#         panorama()
+     with tabs[1]:
+         panorama()
 
 # Pre-process Image
 def preProcessImg(img):
